24729755a53af87efb3cc6515182e03a8fbac34b
Muthu Kandasamy <muthu@lightningcloudconsultancy.com>
2021-01-05 19:39:33 +0000

    Merged PR 18764: 107119 [Contracts TD] Contract approver - Create base solution using azure function solution template
    Initial commit - boilerplate code based on solution template. There are no additional changes to this PR.
    
    Related work items: #107119

---
+++ Pds.Contracts.Approver/Pds.Contracts.Approver.Func/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Pds.Contracts.Approver.Func;
+using Pds.Contracts.Approver.Services.DependencyInjection;
+
+// See: https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Pds.Contracts.Approver.Func
+{
+    /// <summary>
+    /// The startup class.
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        /// <inheritdoc/>
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddFeatureServices();
+        }
+    }
+}
